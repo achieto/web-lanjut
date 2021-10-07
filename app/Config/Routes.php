86fcf563496cd1b27ab4b92a1f925da3e61ec740
achieto <anggie.t17210@gmail.com>
2021-10-07 22:15:40 +0700

    Update Project

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,8 +7,7 @@ $routes = Services::routes();
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
-{
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
 	require SYSTEMPATH . 'Config/Routes.php';
 }
 
@@ -43,6 +42,8 @@ $routes->get('/', function () {
 	echo view('layouts/footer');
 });
 
+$routes->get('/register', 'Templating::register');
+$routes->post('/save', 'Templating::save');
 $routes->get('/posts', 'PostController::index');
 $routes->get('/admin', 'Templating::index');
 $routes->get('/about', function () {
@@ -67,7 +68,6 @@ $routes->get('/about', function () {
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
-{
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
--- app/Controllers/Templating.php
+++ app/Controllers/Templating.php
@@ -3,16 +3,42 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class Templating extends BaseController
 {
+    public function __construct()
+    {
+        $this->userModel = new userModel();
+    }
+
     public function index() 
     {
         $data = [
-            'title' => 'Blog - Post'
+            'title' => 'Blog - Admin'
         ];
 
         return view('v_admin');
     }
+
+    public function register()
+    {
+        $data = [
+            'title' => 'Blog - Register'
+        ];
+
+        return view('v_register', $data);
+    }
+
+    public function save()
+    {
+        $request = service('request');
+        $data = [
+            'fullname' => $request->getVar('fullname'),
+            'email'    => $request->getVar('email'),
+            'password' => $request->getVar('password'),
+        ];
+        $this->userModel->insert($data);
+        return redirect()->to('/register');
+    }
 }
-?>
